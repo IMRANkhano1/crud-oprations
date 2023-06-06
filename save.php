98676f73717212ab565f94729c061765db660c0f
IMRAN SHAKIL S <134833198+IMRANkhano1@users.noreply.github.com>
2023-06-06 21:48:03 +0530

    Update save.php

--- save.php
+++ save.php
@@ -1,56 +1,21 @@
-
 <?php
-include "configfd.php";
-$name=$_POST['ename'];
-$regno=$_POST['regno'];
-$addr=$_POST['addr'];
+require "config.php";
 
-$query="insert into sample values('$name',$regno,'$addr')";
+//$id = $_POST['id'];
+$uname = $_POST['username'];
+$email = $_POST['email'];
+$pass = $_POST['password'];
+$phone = $_POST['phone'];
 
-// if(isset($_POST['submit']))
-// {
-// if($res=mysqli_query($con,$query))
-//  {
-// 	echo"data saved";
-// }
-// else
-// {
-// 	echo"data eror";
-// }
-// }
+$query = "INSERT INTO `crud-php` (username, email,password, phone) 
+          VALUES ('$uname', '$email','$pass',$phone)";
 
-
-if(isset($_POST['submit']))
-{
-if($res=mysqli_query($con,$query))
- {
-	echo"<script>alert('data saved');</script>";
-	// header('location:form2.php');
-}
-else
-{
-	echo"<script>alert('data eror');</script>";
-}
-}
-if(isset($_POST['tableview']))
- {
-    $query2="select*from sample";
-
-    $result=mysqli_query($con,$query2);
-    
-    echo"<table border=1>";
-    echo"<tr><th>name</th>";
-    echo"<th>regno</th>";
-    echo "<th>Addr</th> ";
-    echo"<th>update</th></tr>";
-    while($row=mysqli_fetch_array($result))
-    {
-    echo"<tr><td>$row[0]</td>";
-    echo"<td>$row[1]</td>";
-    echo"<td>$row[2]</td>";
-    echo "<td><input type='submit' name='update' value='update'></td></tr>";
-
+if (isset($_POST['submit'])) {
+    if ($res = mysqli_query($conn, $query)) {
+        echo "<script>alert('Stored success');window.location.href='view.php';</script>";
+    } else {
+        echo "<script>alert('Stored error');window.location.href='view.php';</script>";
     }
-    echo "</table>";
 }
+
 ?>
